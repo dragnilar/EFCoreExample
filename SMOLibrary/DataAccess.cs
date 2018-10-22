5fef230fa6d4667d8c1623cb6987c0cb3865a859
Great Dead Cthulhu <dragnilar@gmail.com>
2018-10-22 00:43:07 -0400

    Added an SMO library as an alternate data source

---
+++ SMOLibrary/DataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace SMOLibrary
+{
+    public static class DataAccess
+    {
+
+        public static Database GetDatabase(Server server)
+        {
+            return new Database(server, "EF6DbContextDb");
+        }
+
+        public static Server GetServer()
+        {
+            var server = new Server();
+            var connectionBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "(localdb)\\MSSQLLocalDB",
+                InitialCatalog = "EF6DbContextDb",
+                IntegratedSecurity = true,
+                MultipleActiveResultSets = true
+            };
+
+            server.ConnectionContext.ConnectionString = connectionBuilder.ToString();
+
+            return server;
+        }
+    }
+}
---
+++ SMOLibrary/DatabaseBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace SMOLibrary
+{
+    public class DatabaseBuilder
+    {
+        public void CreateDatabaseIfNotExists()
+        {
+            var server = DataAccess.GetServer();
+            if (!server.Databases.Contains("EF6DbContextDb"))
+            {
+                var database = DataAccess.GetDatabase(server);
+                database.Create();
+                database.Tables.Add(GenerateMonsterTable(database));
+                database.Tables.Add(GenerateWeaponsTable(database));
+                
+            }
+        }
+
+
+        private Table GenerateMonsterTable(Database database)
+        {
+            var table = new Table(database, "Monsters");
+            var idColumn = new Column
+            {
+                Parent = table,
+                Identity = true,
+                DataType = DataType.Int,
+                Name = "MonsterId"
+
+            };
+            table.Columns.Add(idColumn);
+            table.Columns.Add(new Column(table, "MonsterName", DataType.NVarChar(1000)));
+            table.Columns.Add(new Column(table, "MonsterType", DataType.NVarChar(250)));
+            var pkIndex = new Index(table, "PK_Monsters")
+            {
+                IsUnique = true,
+                IndexKeyType = IndexKeyType.DriPrimaryKey,
+                IsClustered = true
+            };
+            pkIndex.IndexedColumns.Add(new IndexedColumn(pkIndex, idColumn.Name));
+
+            return table;
+        }
+
+        private Table GenerateWeaponsTable(Database database)
+        {
+            var table = new Table(database, "Weapons");
+            var idColumn = new Column
+            {
+                Parent = table,
+                Identity = true,
+                DataType = DataType.Int,
+                Name = "WeaponId"
+
+            };
+            table.Columns.Add(idColumn);
+            table.Columns.Add(new Column(table, "WeaponName", DataType.NVarChar(1000)));
+            table.Columns.Add(new Column(table, "WeaponType", DataType.NVarChar(250)));
+            var pkIndex = new Index(table, "PK_Weapons")
+            {
+                IsUnique = true,
+                IndexKeyType = IndexKeyType.DriPrimaryKey,
+                IsClustered = true
+            };
+            pkIndex.IndexedColumns.Add(new IndexedColumn(pkIndex, idColumn.Name));
+
+            return table;
+        }
+
+
+
+    }
+}
